c5415cc6395c2caea8dab523563b88df77f8cd99
Jon <jonathan.nut@gmail.com>
2018-02-18 22:05:32 +0000

    Added CallLog links

--- Spec.php
+++ Spec.php
@@ -43,13 +43,19 @@
                   Edit Ticket
                 </a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link" href="CallLog.php">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                Call Log
+                </a>
+              </li>
             </ul>
           </div>
         </nav>
 
         <main role="main" class="col-md-6 ml-sm-auto col-lg-10 pt-3 px-4 bg-light">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h1 class="h2">Add Ticket</h1>
+            <h1 class="h2">Specialists</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
             </div>
           </div>  
