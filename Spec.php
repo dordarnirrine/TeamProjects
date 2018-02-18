fc251c6b902bf2c0cee320f75288f559359e5db2
Jon <jonathan.nut@gmail.com>
2018-02-18 13:31:49 +0000

    Updated Spec.php

---
+++ Spec.php
@@ -0,0 +1,96 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+    </head>
+
+    <body>
+    <div class="row">
+        <nav class="col-sm-2 d-none d-md-block bg-dark sidebar h-100">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#"> <h1 class="text-light col-sm"> Make-It All </h1> </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#"> </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#"> </a>
+              </li>
+            </ul>
+
+            <h5 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              <span>Navigation</span>
+              <a class="d-flex align-items-center text-muted" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
+              </a>
+            </h5>
+          </div>
+        </nav>
+
+        <main role="main" class="col-md-6 ml-sm-auto col-lg-10 pt-3 px-4 bg-light">
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+            <h1 class="h2">Login</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+            </div>
+          </div>  
+          <div class="col h-25"> </div>
+          <div class="col-md-8 ml-sm-auto row align-items-center">
+            <div class="wrapper">
+            <table id="content-tickets">
+                
+                <tr>
+                    <th>Company</th>
+                    <th>Contact</th>
+                    <th>Country</th>
+                    <th> Contact Number</th>
+                    <th> Specialisation </th>
+                </tr>
+                <tr>
+                    <td>Alfreds Futterkiste</td>
+                    <td>Maria Anders</td>
+                    <td>Germany</td>
+                    <td> +449098790735</td>
+                    <td> Networks and Communication </td>
+                </tr>
+                <tr>
+                    <td>Centro comercial Moctezuma</td>
+                    <td>Francisco Chang</td>
+                    <td>Mexico</td>
+                    <td> +449098790022 </td> 
+                    <td> Hardware </td>
+                </tr>
+                <tr>
+                    <td>Ernst Handel</td>
+                    <td>Roland Mendel</td>
+                    <td>Austria</td>
+                    <td> +449098790370</td>
+                    <td> Software </td>
+                </tr>
+                <tr>
+                    <td>Island Trading</td>
+                    <td>Helen Bennett</td>
+                    <td>UK</td>
+                    <td> +449098790292</td>
+                    <td> Data Security </td>
+                </tr>
+                <tr>
+                    <td>Laughing Bacchus Winecellars</td>
+                    <td>Jeremy Smith</td>
+                    <td>Canada</td>
+                    <td> +449098790000</td>
+                    <td> System Maintenance</td>
+                </tr>
+                <tr>
+                    <td>Magazzini Alimentari Riuniti</td>
+                    <td>Martin Connor</td>
+                    <td>Italy</td>
+                    <td> +449098790843</td>
+                    <td> System Admin </td>
+                </tr>
+                </table>
+
+            </div>
+          </div>
+    </body>
+</html>
